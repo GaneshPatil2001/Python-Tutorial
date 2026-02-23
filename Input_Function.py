c17aa6a1d0d1c97a333adc1730eac16dc9c71003
GaneshPatil2001 <ganupatilchopda@gmail.com>
2026-02-23 13:21:30 +0530

    Updating some files and add String.py file

--- Input_Function.py
+++ Input_Function.py
@@ -3,7 +3,17 @@ name = input("Enter your name: ")
 age = input("Enter your age: ") 
 print(f"Hello {name}, you are {age} years old.")
 
-no1= float(input("Enter first number: "))
-no2= float(input("Enter second number: "))
+no1= int(input("Enter first number: "))
+no2= int(input("Enter second number: "))
 sum = no1 + no2
-print(f"The sum of {no1} and {no2} is: {int(sum)}")
+print(f"The sum of {no1} and {no2} is: {sum}")
+#Note: The input() function always returns a string, 
+# so we need to convert it to the appropriate data type (like int or float) 
+# if we want to perform mathematical operations.
+
+if no1 > no2:
+    print(f"{no1} is greater than {no2}")
+elif no1 < no2:
+    print(f"{no2} is greater than {no1}")
+else:
+    print(f"{no1} and {no2} are equal")
--- Variable_Data_Type.py
+++ Variable_Data_Type.py
@@ -70,7 +70,7 @@ print(x)  # Output: 1.0
 
 #Arithmetic operators
 a = 10
-b = 5
+b = 3
 print(a + b)  # Addition: 10 + 3 = 13
 print(a - b)  # Subtraction: 10 - 3 = 7
 print(a * b)  # Multiplication: 10 * 3 = 30
@@ -78,3 +78,4 @@ print(a / b)  # Division: 10 / 3 = 3.333...
 print(a // b) # Floor Division: 10 // 3 = 3
 print(a % b)  # Modulus: 10 % 3 = 1
 print(a ** b) # Exponentiation: 10 ** 3 = 1000  
+
