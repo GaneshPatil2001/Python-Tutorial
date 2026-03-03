4199610fd490010b21c1a2aa4ca24763df842c29
GaneshPatil2001 <ganupatilchopda@gmail.com>
2026-03-03 11:42:54 +0530

    Uploading proect on basics

---
+++ Projects/Snake Water Gun/main.py
@@ -0,0 +1,25 @@
+# 1 for snake
+# -1 for water
+# 0 for gun
+
+import random as rd
+
+computer = rd.choice([-1, 0, 1])
+user = int(input("Enter 1 for snake, -1 for water, and 0 for gun: "))
+
+userDict = {1: "snake", -1: "water", 0: "gun"}
+
+if user not in userDict:
+    print("Invalid input!")
+else:
+    print("Computer chose:", userDict[computer])
+    print("User chose:", userDict[user])
+
+    if computer == user:
+        print("It's a tie!")
+    elif (computer == -1 and user == 1) or \
+         (computer == 1 and user == 0) or \
+         (computer == 0 and user == -1):
+        print("Computer wins!")
+    else:
+        print("User wins!")
