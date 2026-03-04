bda760b47d25a0e77ce0ade6730f6fdcb553b4ab
GaneshPatil2001 <ganupatilchopda@gmail.com>
2026-03-04 20:57:01 +0530

    Adding problem on file

--- File Manipulation/Auto_Close.py
+++ File Manipulation/Auto_Close.py
@@ -4,4 +4,22 @@ with open("Demo_File.txt", "r+") as f:
         print("Location is already mentioned in the file.")
     print(data)
 
-    
+
+# Problem
+import random as rd
+score = rd.randint(0, 100)
+print("Your score is", score)
+with open("Score.txt", "r+") as f:
+    high_score = f.read()
+    if high_score == "":
+        high_score = 0
+    else:        
+        high_score = int(high_score)
+        print("The current high score is", high_score)
+
+    if score > high_score:
+        f.seek(0)
+        f.write(str(score))
+        print("Congratulations! You have a new high score of", score)
+    else:        print("Your score is", score, "but the high score is", high_score)
+
