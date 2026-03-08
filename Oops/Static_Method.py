1e4807476863c95e09e96d2153df17e9c35ba44c
GaneshPatil2001 <ganupatilchopda@gmail.com>
2026-03-08 16:13:41 +0530

    adding some examples on oops

--- Oops/Static_Method.py
+++ Oops/Static_Method.py
@@ -1,12 +1,25 @@
 class student:
-    def __init__(self, name, age):
-        self.name=name
+    def __init__(self,__name__, age):
+        self.__name__=__name__
         self.age=age
     
     @staticmethod
     def display():
         print("hii")
+    
+    def _showName_(self):
+        print("From inside showname method")
+        print(self.__name__)
 
 s1=student("Ganesh",20)
 
 s1.display()
+
+student("Prashant",21).display()
+
+student("Ishwar",24)._showName_()
+
+s2=student("Shubham",24)
+print("Use class attribute here",s2.__name__)
+s2.__name__="Mayur"       #Instance variable is created
+print("Now modification within instance variable : ",s2.__name__)
